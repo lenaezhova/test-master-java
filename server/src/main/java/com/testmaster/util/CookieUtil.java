c88e1a93b013e599df7b5b45c0f52a53d5c9e2cf
Elena Mitina <119130068+lenaezhova@users.noreply.github.com>
2025-04-08 12:33:37 +0400

    delete unused

--- server/src/main/java/com/testmaster/util/CookieUtil.java
+++ server/src/main/java/com/testmaster/util/CookieUtil.java
@@ -8,27 +8,6 @@ public class CookieUtil {
 
     private static final String REFRESH_TOKEN_NAME = "refreshToken";
     private static final int REFRESH_TOKEN_EXPIRATION = 30 * 24 * 60 * 60; // 30 дней
-    private static final String DOMAIN = System.getenv("CLIENT_URL_DOMAIN");
-
-//    public static void addRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
-//        Cookie refreshCookie = new Cookie(REFRESH_TOKEN_NAME, refreshToken);
-//        refreshCookie.setHttpOnly(true);
-//        refreshCookie.setSecure(true);
-//        refreshCookie.setMaxAge(REFRESH_TOKEN_EXPIRATION);
-//        refreshCookie.setPath("/");
-//        refreshCookie.setDomain(DOMAIN);
-//
-//        String cookieHeader = String.format(
-//                "%s=%s; Max-Age=%d; Path=/; Domain=%s; Secure; HttpOnly; SameSite=None",
-//                REFRESH_TOKEN_NAME,
-//                refreshToken,
-//                REFRESH_TOKEN_EXPIRATION,
-//                DOMAIN
-//        );
-//        response.setHeader("Set-Cookie", cookieHeader);
-//
-//        response.addCookie(refreshCookie);
-//    }
 
     public static void addRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
         String cookieHeader = String.format(
@@ -37,8 +16,6 @@ public class CookieUtil {
                 refreshToken,
                 REFRESH_TOKEN_EXPIRATION
         );
-
-        System.out.println("CookieHeader :" + cookieHeader);
 
         response.setHeader("Set-Cookie", cookieHeader);
     }
@@ -50,24 +27,6 @@ public class CookieUtil {
         );
         response.setHeader("Set-Cookie", cookieHeader);
     }
-
-//    public static void deleteRefreshTokenCookie(HttpServletResponse response) {
-//        Cookie deleteCookie = new Cookie(REFRESH_TOKEN_NAME, "");
-//        deleteCookie.setHttpOnly(true);
-//        deleteCookie.setSecure(true);
-//        deleteCookie.setMaxAge(0);
-//        deleteCookie.setPath("/");
-//        deleteCookie.setDomain(DOMAIN);
-//
-//        String cookieHeader = String.format(
-//                "%s=; Max-Age=0; Path=/; Domain=%s; Secure; HttpOnly; SameSite=None",
-//                REFRESH_TOKEN_NAME,
-//                DOMAIN
-//        );
-//        response.setHeader("Set-Cookie", cookieHeader);
-//
-//        response.addCookie(deleteCookie);
-//    }
 
     public static String getCookieValue(HttpServletRequest request, String name) {
         if (request.getCookies() == null) {
